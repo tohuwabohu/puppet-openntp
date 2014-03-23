9332e5f2bef691de81977c1559d4e4aa07d7723c
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-03-23 22:43:45 +1100

    Add test cases for server parameter

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -6,9 +6,13 @@ describe 'openntp' do
   describe 'by default' do
     let(:params) { { } }
 
-    it { should contain_file('/etc/openntpd/ntpd.conf').with_ensure('present') }
     it { should contain_package('openntpd').with_ensure('installed') }
     it { should contain_service('openntpd').with_ensure('running').with_enable(true) }
+    it { should contain_file('/etc/openntpd/ntpd.conf').with_ensure('present') }
+    it { should contain_file('/etc/openntpd/ntpd.conf').with_content(/server 0.debian.pool.ntp.org/) }
+    it { should contain_file('/etc/openntpd/ntpd.conf').with_content(/server 1.debian.pool.ntp.org/) }
+    it { should contain_file('/etc/openntpd/ntpd.conf').with_content(/server 2.debian.pool.ntp.org/) }
+    it { should contain_file('/etc/openntpd/ntpd.conf').with_content(/server 3.debian.pool.ntp.org/) }
   end
 
   describe 'with custom version' do
@@ -23,6 +27,13 @@ describe 'openntp' do
     it { should contain_package('openntpd').with_ensure('absent') }
     it { should_not contain_file('/etc/openntpd/ntpd.conf') }
     it { should_not contain_service('openntpd') }
+  end
+
+  describe 'with custom server' do
+    let(:params) { {:server => ['ntp.example.org']} }
+
+    it { should contain_file('/etc/openntpd/ntpd.conf').with_content(/server ntp.example.org/) }
+    it { should_not contain_file('/etc/openntpd/ntpd.conf').with_content(/server 0.debian.pool.ntp.org/) }
   end
 
   describe 'disable service' do
