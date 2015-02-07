71afa77ebc286af1aa8ea6cb9e793e6a175d1f66
Martin Meinhold <Martin.Meinhold@gmx.de>
2015-02-07 22:14:42 +1100

    Add support for Ubuntu 12.04 and 14.04

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,6 +12,28 @@
 #
 class openntp::install inherits openntp {
 
+  if $openntp::ensure != absent and $::lsbdistid == 'Ubuntu' {
+    # Installing openntpd will automatically remove the ntp package. But on Ubuntu, just removing the package will leave
+    # the apparmor profile behind. This still active apparmor profile will then prevent the openntpd service from
+    # accessing its own configuration file. The solution is to purge the package (to get rid of the apparmor profile)
+    # and then reload the apparmor service to pickup the configuration change.
+    #
+    # see https://bugs.launchpad.net/ubuntu/+source/openntpd/+bug/458061 for more information
+
+    package { 'ntp':
+      ensure => purged,
+      before => Package[$openntp::package_name],
+    }
+
+    exec { 'reload apparmor service after ntp removal':
+      command     => 'service apparmor reload',
+      path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+      refreshonly => true,
+      subscribe   => Package['ntp'],
+      before      => Package[$openntp::package_name],
+    }
+  }
+
   package { $openntp::package_name:
     ensure => $openntp::ensure,
   }
