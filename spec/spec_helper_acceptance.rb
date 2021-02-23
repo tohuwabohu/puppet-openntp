2adc855ed0ec41fe136f78836858f0976196ee71
Martin Meinhold <Martin.Meinhold@gmx.de>
2021-02-23 20:21:33 +0100

    Update stdlib to latest version

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,7 +6,7 @@ require 'beaker/module_install_helper'
 # collection: puppet - latest, puppet6 is the current version
 install_puppet_agent_on(hosts, {:puppet_collection => 'puppet' } )
 install_module_on(hosts)
-install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 6.2.0')
+install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 6.6.0')
 
 RSpec.configure do |c|
   c.formatter = :documentation
